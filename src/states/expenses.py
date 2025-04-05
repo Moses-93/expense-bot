f6e9ba9b1f75da0eb66632c153338882ee456668
Moses-93 <moieseenko2001@gmail.com>
2025-04-05 15:06:06 +0300

    Feat: Add states for deleting and updating expenses, and rename AND_DATE to END_DATE

--- src/states/expenses.py
+++ src/states/expenses.py
@@ -13,4 +13,14 @@ class GetExpensesReportStates(StatesGroup):
     """States for getting an expenses report."""
 
     START_DATE = State()
-    AND_DATE = State()
+    END_DATE = State()
+
+
+class DeleteExpenseState(StatesGroup):
+
+    EXPENSE_ID = State()
+
+
+class UpdateExpenseState(StatesGroup):
+
+    EXPENSE_ID = State()
