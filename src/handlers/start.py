1db975b28b69799deb15007564875c1d8fab5f66
Moses-93 <moieseenko2001@gmail.com>
2025-04-05 15:05:09 +0300

    Feat: Implement start command handler for expense management bot

---
+++ src/handlers/start.py
@@ -0,0 +1,16 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from src.keyboards.main_user_keyboard import MainUserKeyboard
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def start_command(message: Message):
+    """Handle the /start command and the start button in the main menu."""
+    await message.answer(
+        "Привіт! Я бот для управління витратами." "Виберіть дію з меню нижче.",
+        reply_markup=MainUserKeyboard().main_menu(),
+    )
