770ddd84fb718af51e871f7afdf64290ab2758f5
Moses-93 <moieseenko2001@gmail.com>
2025-04-13 18:53:01 +0300

    chore: Add custom exception classes for expense handling

---
+++ src/core/exceptions/expense_exc.py
@@ -0,0 +1,25 @@
+class ExpenseError(Exception):
+    def __init__(self, status_code: int, message: str = "Unexpected error"):
+        self.status_code = status_code
+        self.message = message
+        super().__init__(message)
+
+
+class ExpenseNotFoundError(ExpenseError):
+    pass
+
+
+class ExpenseGetReportError(ExpenseError):
+    pass
+
+
+class ExpenseCreateError(ExpenseError):
+    pass
+
+
+class ExpenseUpdateError(ExpenseError):
+    pass
+
+
+class ExpenseDeleteError(ExpenseError):
+    pass
