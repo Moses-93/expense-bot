c1cc476ff78cc1e3f7914c7121311627f730787a
Moses-93 <moieseenko2001@gmail.com>
2025-04-20 19:08:34 +0300

    refactor: Update ExpenseCreateHandler to improve field handling and validation logic

--- src/handlers/expense/handlers/create.py
+++ src/handlers/expense/handlers/create.py
@@ -7,14 +7,31 @@ from src.states.expenses import AddExpenseStates
 from src.services.expense import expense_service as exp_service
 from src.handlers.expense.base_handler import BaseExpenseHandler
 from src.handlers.expense.messages import ADD_EXPENSE_MESSAGES
-
+from src.utils.expense_validators import (
+    amount_validator,
+    date_validator,
+    name_validator,
+)
 
 logger = logging.getLogger(__name__)
 
 
 class ExpenseCreateHandler(BaseExpenseHandler):
     def __init__(self, api_client: APIClient):
-        super().__init__(api_client, ADD_EXPENSE_MESSAGES)
+        super().__init__(
+            ADD_EXPENSE_MESSAGES,
+            {
+                "title": name_validator,
+                "date": date_validator,
+                "amount": amount_validator,
+            },
+            {
+                "title": "invalid_title",
+                "date": "invalid_date",
+                "amount": "invalid_uah_amount",
+            },
+        )
+        self.api_client = api_client
 
     async def handle_add_expense(self, message: Message, state: FSMContext):
         await state.clear()
@@ -22,28 +39,21 @@ class ExpenseCreateHandler(BaseExpenseHandler):
         await message.answer(self.messages.get("start"), parse_mode="Markdown")
 
     async def handle_set_expense_name(self, message: Message, state: FSMContext):
-        success = await self._handle_field_input(
-            message,
-            state,
-            field="name",
+        success = await self._handle(
+            message, state, "title", AddExpenseStates.ADD_EXPENSE_DATE
         )
         if success:
-            await state.set_state(AddExpenseStates.ADD_EXPENSE_DATE),
-
-        await message.answer(self.messages.get("set_date"), parse_mode="Markdown")
+            await message.answer(self.messages.get("set_date"), parse_mode="Markdown")
 
     async def handle_set_expense_date(self, message: Message, state: FSMContext):
-        success = await self._handle_field_input(
-            message,
-            state,
-            field="date",
+        success = await self._handle(
+            message, state, "date", AddExpenseStates.ADD_EXPENSE_AMOUNT
         )
         if success:
-            await state.set_state(AddExpenseStates.ADD_EXPENSE_AMOUNT),
-        await message.answer(self.messages.get("set_amount"), parse_mode="Markdown")
+            await message.answer(self.messages.get("set_amount"), parse_mode="Markdown")
 
     async def handle_set_expense_amount(self, message: Message, state: FSMContext):
-        await self._handle_field_input(message, state, field="uah_amount")
+        await self._handle(message, state, field="amount", next_state=None)
 
         await self._finalize_creation(message, state)
 
@@ -56,7 +66,7 @@ class ExpenseCreateHandler(BaseExpenseHandler):
         created_expense = await service.create(data)
         await message.answer(
             self.messages.get("success_create").format(
-                name=created_expense.get("name", "-"),
+                name=created_expense.get("title", "-"),
                 date=created_expense.get("date", "-"),
                 amount=created_expense.get("uah_amount", "-"),
             )
