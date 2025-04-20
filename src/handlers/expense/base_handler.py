b33faaa0e35efa2ade0f3e5e882fd7bbbed93f38
Moses-93 <moieseenko2001@gmail.com>
2025-04-20 19:07:48 +0300

    refactor: Improve the versatility of the base handler. Add functionality to save the context of states

--- src/handlers/expense/base_handler.py
+++ src/handlers/expense/base_handler.py
@@ -1,44 +1,53 @@
+from typing import Callable, Dict, Optional, Tuple, Union
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State
 
-from src.services.api_client import APIClient
 from src.utils.message_manager import MessageManager
-from src.utils.expense_validators import (
-    date_validator,
-    amount_validator,
-    name_validator,
-)
 
 
 class BaseExpenseHandler:
-    VALIDATORS = {
-        "name": name_validator,
-        "date": date_validator,
-        "uah_amount": amount_validator,
-    }
 
-    def __init__(self, api_client: APIClient, messages: dict):
-        self.api_client = api_client
+    def __init__(
+        self,
+        messages: Dict[str, str],
+        validators: Dict[str, Callable],
+        error_keys: Dict[str, str],
+    ):
+        self._validators = validators
+        self._error_keys = error_keys
         self.messages = MessageManager(messages)
 
-    def _get_validator(self, field: str):
-        return self.VALIDATORS.get(field)
-
-    async def _handle_field_input(
+    async def _handle(
         self,
         message: Message,
         state: FSMContext,
-        field: str,
+        field: Optional[str] = None,
+        next_state: Optional[State] = None,
     ) -> bool:
+
         value = message.text
+        if value and field:
+            ok, error_key, value = self._validate(field, value)
 
-        if value:
-            valid_value = self._get_validator(field)(value)
-
-            if not valid_value:
-                await message.answer(self.messages.get(f"invalid_{field}"))
+            if not ok:
+                await message.answer(self.messages.get(error_key))
                 return False
 
-            await state.update_data({field: valid_value})
-
+            await state.update_data({field: value})
+        if next_state:
+            await state.set_state(next_state)
         return True
+
+    def _validate(
+        self, field: str, raw_value: str
+    ) -> Tuple[bool, Union[str, None], any]:
+
+        validator = self._validators.get(field)
+        if not validator:
+            raise ValueError(f"Unknown field: {field}")
+
+        result = validator(raw_value)
+        if result is None:
+            return False, self._error_keys[field], None
+        return True, None, result
