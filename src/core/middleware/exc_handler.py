5621c16707fde54c8684ebe8f1c65e2eda870c48
Moses-93 <moieseenko2001@gmail.com>
2025-04-23 13:16:46 +0300

    feat: Add ErrorHandlingMiddleware for improved error response handling

---
+++ src/core/middleware/exc_handler.py
@@ -0,0 +1,27 @@
+import logging
+from aiogram import BaseMiddleware
+from aiogram.types import Message, CallbackQuery
+from typing import Dict, Union
+from core.exceptions import exc_validation
+
+logger = logging.getLogger(__name__)
+
+
+async def send_response(event: Union[Message, CallbackQuery], message: str):
+    if isinstance(event, CallbackQuery):
+        await event.message.answer(text=message)
+    elif isinstance(event, Message):
+        await event.answer(text=message)
+
+
+class ErrorHandlingMiddleware(BaseMiddleware):
+
+    async def __call__(self, handler, event: Union[Message, CallbackQuery], data: Dict):
+        try:
+            return await handler(event, data)
+        except exc_validation.InvalidAmountError as e:
+            await send_response(event, e.message)
+        except exc_validation.InvalidDateError as e:
+            await send_response(event, e.message)
+        except exc_validation.InvalidNameError as e:
+            await send_response(event, e.message)
