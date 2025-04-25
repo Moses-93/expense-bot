998ab7fde9b9f59af3a34ea39e341bb80ed038cc
Moses-93 <moieseenko2001@gmail.com>
2025-04-25 17:45:42 +0300

    refactor: Simplify ExpenseCreateHandler.
    - Remove validation of input data by implementing aiogram filters.
    - Remove inheritance.
    - Simplify the creation of an expense.
    - Add DTO to simplify reading and signature of methods.

--- src/handlers/expense/handlers/create.py
+++ src/handlers/expense/handlers/create.py
@@ -1,74 +1,70 @@
 import logging
+from typing import Dict
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
-from src.services.api_client import APIClient
 from src.states.expenses import AddExpenseStates
-from src.services.expense import expense_service as exp_service
-from src.handlers.expense.base_handler import BaseExpenseHandler
-from src.handlers.expense.messages import ADD_EXPENSE_MESSAGES
-from src.utils.expense_validators import (
-    amount_validator,
-    date_validator,
-    name_validator,
-)
+from src.services.expense.expense_service import ExpenseMutationService
+from src.utils.message_provider import MessageProvider
+from src.core.messages.enums import AddExpenseMessage
+from src.models.expense_dto import ExpenseDTO
+
 
 logger = logging.getLogger(__name__)
 
 
-class ExpenseCreateHandler(BaseExpenseHandler):
-    def __init__(self, api_client: APIClient):
-        super().__init__(
-            ADD_EXPENSE_MESSAGES,
-            {
-                "title": name_validator,
-                "date": date_validator,
-                "amount": amount_validator,
-            },
-            {
-                "title": "invalid_title",
-                "date": "invalid_date",
-                "amount": "invalid_uah_amount",
-            },
-        )
-        self.api_client = api_client
+class ExpenseCreateHandler:
+    def __init__(
+        self,
+        message_provider: MessageProvider[AddExpenseMessage],
+        mutation_service: ExpenseMutationService,
+    ):
+        self.message_provider = message_provider
+        self.mutation_service = mutation_service
 
-    async def handle_add_expense(self, message: Message, state: FSMContext):
+    async def handle_start(self, message: Message, state: FSMContext):
         await state.clear()
-        await state.set_state(AddExpenseStates.ADD_EXPENSE_NAME)
-        await message.answer(self.messages.get("start"), parse_mode="Markdown")
-
-    async def handle_set_expense_name(self, message: Message, state: FSMContext):
-        success = await self._handle(
-            message, state, "title", AddExpenseStates.ADD_EXPENSE_DATE
-        )
-        if success:
-            await message.answer(self.messages.get("set_date"), parse_mode="Markdown")
-
-    async def handle_set_expense_date(self, message: Message, state: FSMContext):
-        success = await self._handle(
-            message, state, "date", AddExpenseStates.ADD_EXPENSE_AMOUNT
-        )
-        if success:
-            await message.answer(self.messages.get("set_amount"), parse_mode="Markdown")
-
-    async def handle_set_expense_amount(self, message: Message, state: FSMContext):
-        await self._handle(message, state, field="amount", next_state=None)
-
-        await self._finalize_creation(message, state)
-
-    async def _finalize_creation(self, message: Message, state: FSMContext):
-        data = await state.get_data()
-        logger.debug(f"{data=}")
-        service = exp_service.ExpenseMutationService(
-            self.api_client, message.from_user.id
-        )
-        created_expense = await service.create(data)
+        await state.set_state(AddExpenseStates.TITLE)
         await message.answer(
-            self.messages.get("success_create").format(
-                name=created_expense.get("title", "-"),
-                date=created_expense.get("date", "-"),
-                amount=created_expense.get("uah_amount", "-"),
-            )
+            self.message_provider.get(AddExpenseMessage.START), parse_mode="Markdown"
         )
+
+    async def handle_set_expense_name(
+        self, message: Message, state: FSMContext, title: str
+    ):
+        await state.update_data(title=title)
+        await state.set_state(AddExpenseStates.DATE)
+        await message.answer(
+            self.message_provider.get(AddExpenseMessage.SET_DATE), parse_mode="Markdown"
+        )
+
+    async def handle_set_expense_date(
+        self, message: Message, state: FSMContext, date: str
+    ):
+        await state.update_data(date=date)
+
+        await state.set_state(AddExpenseStates.AMOUNT)
+        await message.answer(
+            self.message_provider.get(AddExpenseMessage.SET_AMOUNT),
+            parse_mode="Markdown",
+        )
+
+    async def handle_set_expense_amount(
+        self, message: Message, state: FSMContext, amount: float
+    ):
+        state_data = await state.update_data(amount=amount)
         await state.clear()
+
+        await self._finalize_creation(message, state_data)
+
+    async def _finalize_creation(self, message: Message, state_data: Dict):
+        expense_data = ExpenseDTO(**state_data)
+        await self.mutation_service.create(message.from_user.id, expense_data)
+        await message.answer(
+            self.message_provider.get(AddExpenseMessage.SUCCESS).format(
+                name=expense_data.title,
+                date=expense_data.date.isoformat(),
+                amount=expense_data.amount,
+            ),
+            parse_mode="Markdown",
+        )
