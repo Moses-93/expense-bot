6d6b2e95cea754670f56bb8f0c3bd89a37f04548
Moses-93 <moieseenko2001@gmail.com>
2025-04-25 18:14:29 +0300

    refactor: Update the ExpenseDeleteHandler.
    - Replace MessageManager with MessageProvider.
    - Remove the direct dependency on APIClient.
    - Add a method to get the report in JSON.

--- src/handlers/expense/handlers/delete.py
+++ src/handlers/expense/handlers/delete.py
@@ -1,46 +1,52 @@
 import logging
 from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
 
 from src.keyboards.display_data_keyboard import DisplayData
-from src.services.api_client import APIClient
-
 from src.states.expenses import DeleteExpenseStates
-from src.utils.message_manager import MessageManager
+from src.utils.message_provider import MessageProvider
 from src.services.expense import (
     expense_service as exp_service,
 )
-from src.handlers.expense.messages import DELETE_EXPENSE_MESSAGES
+from src.core.messages.enums import DeleteExpenseMessage
 
 
 logger = logging.getLogger(__name__)
 
 
 class ExpenseDeleteHandler:
-    def __init__(self, api_client: APIClient):
-        self.api_client = api_client
-        self.messages = MessageManager(DELETE_EXPENSE_MESSAGES)
+    def __init__(
+        self,
+        mutation_service: exp_service.ExpenseMutationService,
+        message_provider: MessageProvider[DeleteExpenseMessage],
+    ):
+        self.mutation_service = mutation_service
+        self.message_provider = message_provider
 
     async def start(self, message: Message, state: FSMContext):
         await state.clear()
-        expense_data = await exp_service.JSONExpenseReportService().execute(
-            message.from_user.id, self.api_client
-        )
-        expense_keyboard = DisplayData.generate_keyboard(
-            expense_data, ("name", "date", "uah_amount"), ("id",)
-        )
+
+        expense_keyboard = await self.get_expense(message.from_user.id)
         await state.set_state(DeleteExpenseStates.EXPENSE_ID)
-        await message.answer(self.messages.get("start"), reply_markup=expense_keyboard)
+        await message.answer(
+            self.message_provider.get(DeleteExpenseMessage.START),
+            reply_markup=expense_keyboard,
+        )
 
     async def handle_delete_expense(self, callback: CallbackQuery, state: FSMContext):
 
-        service = exp_service.ExpenseMutationService(
-            self.api_client, callback.from_user.id
-        )
-        await service.delete(callback.data)
+        await self.mutation_service.delete(callback.data, callback.from_user.id)
 
         await callback.message.answer(
-            self.messages.get("success_delete"), parse_mode="Markdown"
+            self.message_provider.get(DeleteExpenseMessage.SUCCESS),
+            parse_mode="Markdown",
         )
         await state.clear()
+
+    async def get_expense(self, user_id: int):
+        url = exp_service.ExpenseReportURLBuilder("json").build_url()
+        row_expense_data = await self.mutation_service.get(user_id, "json", url)
+        expense_keyboard = DisplayData.generate_keyboard(
+            row_expense_data, ("title", "date", "uah_amount"), ("id",)
+        )
+        return expense_keyboard
