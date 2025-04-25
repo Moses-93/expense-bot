c938261572eb20a9c4874582328e8045c8f0f3d4
Moses-93 <moieseenko2001@gmail.com>
2025-04-25 17:41:32 +0300

    refactor: UpdateExpenseGetReportHandler.
    - Replaced the APIClient dependency with specialized services.
    - Removed inheritance from BaseExpenseHandler.
    - Moved the validation logic to the router through the aiogram filters.
    - Move report formatting to a separate method.
    - Add DTO objects for better readability and simplification of the method signature.

--- src/handlers/expense/handlers/get.py
+++ src/handlers/expense/handlers/get.py
@@ -1,60 +1,59 @@
 import logging
-from aiogram.types import Message
+from typing import Dict
+from aiogram.types import Message, BufferedInputFile
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message
-
-from src.services.api_client import APIClient
 
 from src.states.expenses import GetExpensesReportStates
-from src.utils.expense_validators import date_validator
-from src.services.expense import expense_service as exp_service
-
-from src.handlers.expense.base_handler import BaseExpenseHandler
-from src.handlers.expense.messages import GET_EXPENSE_MESSAGES
-
+from src.services.expense.expense_service import ReportGeneratorService
+from src.utils.message_provider import MessageProvider
+from src.core.messages.enums import GetExpenseMessage
+from src.models.expense_dto import ExpenseReportRequestDTO, ExpenseReportFile
 
 logger = logging.getLogger(__name__)
 
 
-class ExpenseGetReportHandler(BaseExpenseHandler):
-    def __init__(self, api_client: APIClient):
-        super().__init__(api_client, GET_EXPENSE_MESSAGES)
-
-    def _get_validator(self, field: str):
-        if field in ("start_date", "end_date"):
-            return date_validator
-        return super()._get_validator(field)
+class ExpenseGetReportHandler:
+    def __init__(
+        self,
+        report_generator: ReportGeneratorService,
+        message_provider: MessageProvider[GetExpenseMessage],
+    ):
+        self.report_generator = report_generator
+        self._messages = message_provider
 
     async def handle_start(self, message: Message, state: FSMContext):
         await state.clear()
         await state.set_state(GetExpensesReportStates.START_DATE)
-        await message.answer(self.messages.get("start"), parse_mode="Markdown")
+        await message.answer(
+            self._messages.get(GetExpenseMessage.START_DATE), parse_mode="Markdown"
+        )
 
-    async def handle_set_start_date(self, message: Message, state: FSMContext):
-        is_valid = await self._handle_field_input(message, state, "start_date")
-        if is_valid:
-            await state.set_state(GetExpensesReportStates.END_DATE)
-            await message.answer(
-                self.messages.get("set_end_date"), parse_mode="Markdown"
-            )
+    async def handle_set_start_date(
+        self, message: Message, state: FSMContext, date: str
+    ):
+        await state.update_data(start_date=date)
+        await state.set_state(GetExpensesReportStates.END_DATE)
+        await message.answer(
+            self._messages.get(GetExpenseMessage.END_DATE), parse_mode="Markdown"
+        )
 
-    async def handle_set_end_date(self, message: Message, state: FSMContext):
-        is_valid = await self._handle_field_input(message, state, "end_date")
-        if is_valid:
-            await self.handle_generate_expense_report(message, state)
-
-    async def handle_generate_expense_report(self, message: Message, state: FSMContext):
-        data = await state.get_data()
-        start_date = data.get("start_date")
-        end_date = data.get("end_date")
-
+    async def handle_set_end_date(self, message: Message, state: FSMContext, date: str):
+        state_data = await state.update_data(end_date=date)
+        await self.handle_generate_expense_report(message, state_data)
         await state.clear()
 
-        try:
-            service = exp_service.FileExpenseReportService("xlsx", start_date, end_date)
-            report_file = await service.execute(
-                user_id=message.from_user.id, api_client=self.api_client
-            )
-            await message.answer_document(report_file)
-        except ValueError as e:
-            await message.answer(str(e))
+    async def handle_generate_expense_report(
+        self, message: Message, report_request_data: Dict[str, str]
+    ):
+        request_dto = ExpenseReportRequestDTO(**report_request_data)
+
+        report_file_dto = await self.report_generator.generate_report(
+            message.from_user.id, request_dto
+        )
+        await self._send_report(message, report_file_dto)
+
+    async def _send_report(self, message: Message, report_dto: ExpenseReportFile):
+        report_file = BufferedInputFile(
+            file=report_dto.content, filename=report_dto.filename
+        )
+        await message.answer_document(report_file)
